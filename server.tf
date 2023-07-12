ff209bc0c6ae7b78d9c3cc79a20a487881015432
Mayur_Thakkar <66325150+Mayur1062@users.noreply.github.com>
2023-07-12 14:45:58 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -16,9 +16,9 @@ resource "aws_instance" "myawsserver" {
   key_name = "raman-import"
 
   tags = {
-    Name = "Raman-DevOps-batch-server"
+    Name = "Mayur-DevOps-batch-server"
     env = "Production"
-    owner = "Raman Khanna"
+    owner = "Mayur Thakkar"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
